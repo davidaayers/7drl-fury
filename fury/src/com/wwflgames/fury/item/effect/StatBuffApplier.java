594ec3395529f2fb312c6961ea6b6b388d2af6ac
davidaayers <davidaayers@e009b55c-2abc-11df-8aff-a9a8dab06da4>
2010-03-16 02:01:44 +0000

    made title screen better. I should really focus on stuff that makes the game work. Yeah.

--- fury/src/com/wwflgames/fury/item/effect/StatBuffApplier.java
+++ fury/src/com/wwflgames/fury/item/effect/StatBuffApplier.java
@@ -3,7 +3,6 @@ package com.wwflgames.fury.item.effect;
 import com.wwflgames.fury.battle.ItemEffectResult;
 import com.wwflgames.fury.battle.ItemUsageResult;
 import com.wwflgames.fury.mob.Mob;
-import com.wwflgames.fury.mob.Stat;
 
 public class StatBuffApplier extends AbstractBuffApplier {
 
@@ -12,11 +11,11 @@ public class StatBuffApplier extends AbstractBuffApplier {
     }
 
     @Override
-    public void apply(ItemEffect effect, Mob applyTo, ItemUsageResult bag) {
+    public void apply(ItemEffect effect, Mob mob, ItemUsageResult result) {
         StatBuff buff = (StatBuff) effect;
-        applyTo.modifyBattleStatValue(buff.getStat(), buff.getAmount());
+        mob.modifyBattleStatValue(buff.getStat(), buff.getAmount());
 
-        String armorDesc = "{0} armor is increased by {2}";
-        bag.add(new ItemEffectResult(armorDesc, Stat.ARMOR, buff.getAmount(), applyTo, effect));
+        String desc = "{0} " + buff.getStat().getDesc() + " is increased by {2}";
+        result.add(new ItemEffectResult(desc, buff.getStat(), buff.getAmount(), mob, effect));
     }
 }
